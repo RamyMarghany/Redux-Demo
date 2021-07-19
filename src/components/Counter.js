19575c791933a87aec10ea0c5af45536ff139959
Ramy Marghany <ramy.med7t@gmail.com>
2021-07-19 14:02:30 +0200

    initiate the store

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,16 @@
+import { useSelector } from 'react-redux'
 import classes from './Counter.module.css';
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => { };
+
+  // useSelector hook by react-redux firstly it's accept a function as in input to return the required piece of the data from the store that we need, secondly it's create the subscription automatically with the current component, therefore the component will be update the data automatically once it changes in the store 
+  const counter = useSelector(state => state.Counter);
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
